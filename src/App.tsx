e79413414dbadd951a3cbc29a6bd396ad18bea9c
Nils <ossw.nils@gmail.com>
2023-05-19 18:00:35 +0200

    :bug: Use safe areas (#3)

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,23 @@ import React from 'react';
 import { SignIn, Welcome } from './screens';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 const Stack = createNativeStackNavigator();
 
 const App: React.FC = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator 
-        initialRouteName='Welcome' 
-        screenOptions={{ headerShown: false, animation: 'slide_from_right' }}
-      >
-        <Stack.Screen name='Welcome' component={Welcome} />
-        <Stack.Screen name='SignIn' component={SignIn} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Stack.Navigator 
+          initialRouteName='Welcome' 
+          screenOptions={{ headerShown: false, animation: 'slide_from_right' }}
+        >
+          <Stack.Screen name='Welcome' component={Welcome} />
+          <Stack.Screen name='SignIn' component={SignIn} />
+        </Stack.Navigator>
+      </NavigationContainer>  
+    </SafeAreaProvider>
   );
 }
 
