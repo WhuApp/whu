cffc7a6c294eef78093552deed6a9dd325bb86ca
Nils <ossw.nils@gmail.com>
2023-05-20 19:48:33 +0200

    💥 Auth persistence (#7)
    * :heavy_plus_sign: Add @react-native-async-storage/async-storage
    
    * :boom: Add auth persistence

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from 'react';
+import React, { useState } from 'react';
 import {
   Home,
   SignIn,
@@ -8,34 +8,32 @@ import {
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { initializeApp } from 'firebase/app';
-
-const firebaseConfig = {
-  apiKey: 'AIzaSyD8pwH_V0cqNFbZ8ydJuvM3NVBDvYWhFDg',
-  authDomain: 'whuapp-bce7b.firebaseapp.com',
-  projectId: 'whuapp-bce7b',
-  storageBucket: 'whuapp-bce7b.appspot.com',
-  messagingSenderId: '917151787296',
-  appId: '1:917151787296:web:764e32c1bc102bb3a40f45',
-  measurementId: 'G-7K5CHEJ8L2'
-};
+import { User, getAuth } from 'firebase/auth';
 
 const Stack = createNativeStackNavigator();
 
 const App: React.FC = () => {
-  initializeApp(firebaseConfig);
+  const [user, setUser] = useState<User | 'loading'>('loading');
+
+  getAuth().onAuthStateChanged(setUser);
+
+  if (user === 'loading') { return <></> }
 
   return (
     <SafeAreaProvider>
       <NavigationContainer>
         <Stack.Navigator
-          initialRouteName='Welcome'
           screenOptions={{ headerShown: false, animation: 'slide_from_right' }}
         >
-          <Stack.Screen name='Welcome' component={Welcome} />
-          <Stack.Screen name='SignIn' component={SignIn} />
-          <Stack.Screen name='SignUp' component={SignUp} />
-          <Stack.Screen name='Home' component={Home} />
+          {user ? (
+            <Stack.Screen name='Home' component={Home} />
+          ) : (
+            <>
+              <Stack.Screen name='Welcome' component={Welcome} />
+              <Stack.Screen name='SignIn' component={SignIn} />
+              <Stack.Screen name='SignUp' component={SignUp} />
+            </>
+          )}
         </Stack.Navigator>
       </NavigationContainer>
     </SafeAreaProvider>
