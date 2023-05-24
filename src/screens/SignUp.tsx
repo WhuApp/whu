b6d1ef920ee7575d3c74060c78b944ec2bcb4618
gerbernoah <noahgerber100@gmail.com>
2023-05-24 19:08:40 +0200

    correct indents

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -9,10 +9,14 @@ import {
 } from 'react-native';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {RootStackParamList} from '../types';
+<<<<<<< Updated upstream
 import {
     useCreateUserWithEmailAndPassword, useDeleteUser,
     useUpdateProfile
 } from 'react-firebase-hooks/auth';
+=======
+import {useCreateUserWithEmailAndPassword} from 'react-firebase-hooks/auth';
+>>>>>>> Stashed changes
 import {getAuth} from 'firebase/auth';
 import {InsetView, Button} from '../components';
 import {getStyles, Elements} from '../styles';
@@ -28,6 +32,7 @@ const errorByCode = new Map<string, string>([
 type SignUpProps = NativeStackScreenProps<RootStackParamList, 'SignUp'>;
 
 const SignUp: React.FC<SignUpProps> = ({navigation}) => {
+<<<<<<< Updated upstream
     const auth = getAuth();
     const [name, setName] = useState<string>('');
     const [mail, setMail] = useState<string>('');
@@ -36,10 +41,19 @@ const SignUp: React.FC<SignUpProps> = ({navigation}) => {
     const [createUserWithEmailAndPassword, , loading, createUserError] = useCreateUserWithEmailAndPassword(auth);
     const [updateProfile, updating, updateProfileError] = useUpdateProfile(auth);
     const [errorMessage, setErrorMessage] = useState<string | undefined>(null);
+=======
+  const [name, setName] = useState<string>('');
+  const [mail, setMail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [repeatPassword, setRepeatPassword] = useState<string>('');
+  const [createUserWithEmailAndPassword, , loading, error] = useCreateUserWithEmailAndPassword(getAuth());
+  const [pwdNotEqualError, setPwdNotEqualError] = useState<boolean>(false);
+>>>>>>> Stashed changes
 
     const colorScheme = useColorScheme();
     const styles = (element: keyof Elements) => getStyles(element, colorScheme);
 
+<<<<<<< Updated upstream
     const signUp = () => {
         if (password != repeatPassword) {
             setErrorMessage('Passwords do not match');
@@ -109,6 +123,60 @@ const SignUp: React.FC<SignUpProps> = ({navigation}) => {
             <StatusBar style='auto'/>
         </View>
     );
+=======
+  const signUp = () => {
+    setPwdNotEqualError(password != repeatPassword)
+    if (password != repeatPassword) return;
+    createUserWithEmailAndPassword(mail, password).then((user) => {
+      if (user?.user) navigation.navigate('Home');
+    });
+  };
+
+  return (
+    <View style={[styles('page'), {paddingLeft: 15, paddingRight: 15, paddingTop: 80, paddingBottom: 80}]}>
+      <InsetView style={styles('container')}>
+        <Text style={styles('title')}>
+          Sign up for Whu
+        </Text>
+        <View style={{gap: 30}}>
+          <View style={{gap: 10}}>
+            {(error || pwdNotEqualError) &&
+                <Text style={[styles('error'), {alignSelf: 'center'}]}>
+                  {pwdNotEqualError ? 'Passwords do not match' : errorByCode.get(error.code) ?? 'Unknown Error'}
+                </Text>
+            }
+            <View style={styles('inputWrapper')}>
+              <Text style={styles('label')}>Name</Text>
+              <TextInput style={styles('textInput')} onChangeText={setName}/>
+            </View>
+            <View style={styles('inputWrapper')}>
+              <Text style={styles('label')}>E-Mail</Text>
+              <TextInput style={styles('textInput')} onChangeText={setMail}/>
+            </View>
+            <View style={styles('inputWrapper')}>
+              <Text style={styles('label')}>Password</Text>
+              <TextInput style={styles('textInput')} secureTextEntry onChangeText={setPassword}/>
+            </View>
+            <View style={styles('inputWrapper')}>
+              <Text style={styles('label')}>Repeat Password</Text>
+              <TextInput style={styles('textInput')} secureTextEntry onChangeText={setRepeatPassword}/>
+            </View>
+          </View>
+          <Button style={{alignSelf: 'center'}} text='Sign Up' loading={loading} onPress={signUp}/>
+        </View>
+        <View style={{flexDirection: 'row', gap: 3}}>
+          <Text style={styles('text')}>Already have an account?</Text>
+          <Pressable onPress={() => {
+            navigation.navigate('SignIn')
+          }}>
+            <Text style={styles('link')}>Sign in</Text>
+          </Pressable>
+        </View>
+      </InsetView>
+      <StatusBar style='auto'/>
+    </View>
+  );
+>>>>>>> Stashed changes
 };
 
 export default SignUp;
