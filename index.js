c7340bd8fd1eb12f8bde175ebe2b4d811468a9fa
gerbernoah <noahgerber100@gmail.com>
2023-05-24 22:34:53 +0200

    updated project credentials

--- index.js
+++ index.js
@@ -6,14 +6,16 @@ import { getReactNativePersistence } from 'firebase/auth/react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyD8pwH_V0cqNFbZ8ydJuvM3NVBDvYWhFDg',
-  authDomain: 'whuapp-bce7b.firebaseapp.com',
-  projectId: 'whuapp-bce7b',
-  storageBucket: 'whuapp-bce7b.appspot.com',
-  messagingSenderId: '917151787296',
-  appId: '1:917151787296:web:764e32c1bc102bb3a40f45',
-  measurementId: 'G-7K5CHEJ8L2'
+  apiKey: "AIzaSyDNsGUdT6oW-AhUds3ulCrl181IzEXN18I",
+  authDomain: "whuapp-45f31.firebaseapp.com",
+  databaseURL: "https://whuapp-45f31-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "whuapp-45f31",
+  storageBucket: "whuapp-45f31.appspot.com",
+  messagingSenderId: "528481683970",
+  appId: "1:528481683970:web:1a146eef1dbf7822e3351b",
+  measurementId: "G-29S09ZYM0Z"
 };
+
 
 const app = initializeApp(firebaseConfig);
 
