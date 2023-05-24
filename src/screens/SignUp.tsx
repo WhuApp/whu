e473f59f67a98c13e9f5996983f5d6987f4f6168
gerbernoah <noahgerber100@gmail.com>
2023-05-24 19:12:26 +0200

    resolve merge conflict

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,135 +1,68 @@
 import {StatusBar} from 'expo-status-bar';
 import React, {useState} from 'react';
 import {
-    TextInput,
-    View,
-    Text,
-    useColorScheme,
-    Pressable,
+  TextInput,
+  View,
+  Text,
+  useColorScheme,
+  Pressable,
 } from 'react-native';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {RootStackParamList} from '../types';
-<<<<<<< Updated upstream
 import {
-    useCreateUserWithEmailAndPassword, useDeleteUser,
-    useUpdateProfile
+  useCreateUserWithEmailAndPassword, useDeleteUser,
+  useUpdateProfile
 } from 'react-firebase-hooks/auth';
-=======
-import {useCreateUserWithEmailAndPassword} from 'react-firebase-hooks/auth';
->>>>>>> Stashed changes
 import {getAuth} from 'firebase/auth';
 import {InsetView, Button} from '../components';
 import {getStyles, Elements} from '../styles';
 
 const errorByCode = new Map<string, string>([
-    ['auth/missing-password', 'Missing password'],
-    ['auth/invalid-email', 'Invalid E-Mail'],
-    ['auth/weak-password', 'Password too weak'],
-    ['auth/email-already-in-use', 'E-Mail already in use'],
-    ['auth/too-many-requests', 'Too many attempts'],
+  ['auth/missing-password', 'Missing password'],
+  ['auth/invalid-email', 'Invalid E-Mail'],
+  ['auth/weak-password', 'Password too weak'],
+  ['auth/email-already-in-use', 'E-Mail already in use'],
+  ['auth/too-many-requests', 'Too many attempts'],
 ]);
 
 type SignUpProps = NativeStackScreenProps<RootStackParamList, 'SignUp'>;
 
 const SignUp: React.FC<SignUpProps> = ({navigation}) => {
-<<<<<<< Updated upstream
-    const auth = getAuth();
-    const [name, setName] = useState<string>('');
-    const [mail, setMail] = useState<string>('');
-    const [password, setPassword] = useState<string>('');
-    const [repeatPassword, setRepeatPassword] = useState<string>('');
-    const [createUserWithEmailAndPassword, , loading, createUserError] = useCreateUserWithEmailAndPassword(auth);
-    const [updateProfile, updating, updateProfileError] = useUpdateProfile(auth);
-    const [errorMessage, setErrorMessage] = useState<string | undefined>(null);
-=======
+  const auth = getAuth();
   const [name, setName] = useState<string>('');
   const [mail, setMail] = useState<string>('');
   const [password, setPassword] = useState<string>('');
   const [repeatPassword, setRepeatPassword] = useState<string>('');
-  const [createUserWithEmailAndPassword, , loading, error] = useCreateUserWithEmailAndPassword(getAuth());
-  const [pwdNotEqualError, setPwdNotEqualError] = useState<boolean>(false);
->>>>>>> Stashed changes
+  const [createUserWithEmailAndPassword, , loading, createUserError] = useCreateUserWithEmailAndPassword(auth);
+  const [updateProfile, updating, updateProfileError] = useUpdateProfile(auth);
+  const [errorMessage, setErrorMessage] = useState<string | undefined>(null);
 
-    const colorScheme = useColorScheme();
-    const styles = (element: keyof Elements) => getStyles(element, colorScheme);
+  const colorScheme = useColorScheme();
+  const styles = (element: keyof Elements) => getStyles(element, colorScheme);
 
-<<<<<<< Updated upstream
-    const signUp = () => {
-        if (password != repeatPassword) {
-            setErrorMessage('Passwords do not match');
-            return
-        }
-        if (!name.match("^[A-Za-z][A-Za-z0-9_]{2,29}$")) {
-            setErrorMessage('Name is invalid');
-            return
-        }
-        createUserWithEmailAndPassword(mail, password).then((user) => {
-            if (user?.user) {
-                updateProfile({displayName: name}).then(() => {
-                    if (updateProfileError)
-                        useDeleteUser(auth);
-                    else
-                        navigation.navigate('Home');
-                })
-            }
-        });
-        if (createUserError || updateProfileError)
-            setErrorMessage(errorByCode.get(createUserError.code) ?? 'Unknown Error');
-        else
-            setErrorMessage(undefined);
-    };
-
-    return (
-        <View style={[styles('page'), {paddingLeft: 15, paddingRight: 15, paddingTop: 80, paddingBottom: 80}]}>
-            <InsetView style={styles('container')}>
-                <Text style={styles('title')}>
-                    Sign up for Whu
-                </Text>
-                <View style={{gap: 30}}>
-                    <View style={{gap: 10}}>
-                        {errorMessage !== undefined &&
-                            <Text style={[styles('error'), {alignSelf: 'center'}]}>
-                                {errorMessage}
-                            </Text>
-                        }
-                        <View style={styles('inputWrapper')}>
-                            <Text style={styles('label')}>Name</Text>
-                            <TextInput style={styles('textInput')} onChangeText={setName}/>
-                        </View>
-                        <View style={styles('inputWrapper')}>
-                            <Text style={styles('label')}>E-Mail</Text>
-                            <TextInput style={styles('textInput')} onChangeText={setMail}/>
-                        </View>
-                        <View style={styles('inputWrapper')}>
-                            <Text style={styles('label')}>Password</Text>
-                            <TextInput style={styles('textInput')} secureTextEntry onChangeText={setPassword}/>
-                        </View>
-                        <View style={styles('inputWrapper')}>
-                            <Text style={styles('label')}>Repeat Password</Text>
-                            <TextInput style={styles('textInput')} secureTextEntry onChangeText={setRepeatPassword}/>
-                        </View>
-                    </View>
-                    <Button style={{alignSelf: 'center'}} text='Sign Up' loading={loading || updating} onPress={signUp}/>
-                </View>
-                <View style={{flexDirection: 'row', gap: 3}}>
-                    <Text style={styles('text')}>Already have an account?</Text>
-                    <Pressable onPress={() => {
-                        navigation.navigate('SignIn')
-                    }}>
-                        <Text style={styles('link')}>Sign in</Text>
-                    </Pressable>
-                </View>
-            </InsetView>
-            <StatusBar style='auto'/>
-        </View>
-    );
-=======
   const signUp = () => {
-    setPwdNotEqualError(password != repeatPassword)
-    if (password != repeatPassword) return;
+    if (password != repeatPassword) {
+      setErrorMessage('Passwords do not match');
+      return
+    }
+    if (!name.match("^[A-Za-z][A-Za-z0-9_]{2,29}$")) {
+      setErrorMessage('Name is invalid');
+      return
+    }
     createUserWithEmailAndPassword(mail, password).then((user) => {
-      if (user?.user) navigation.navigate('Home');
+      if (user?.user) {
+        updateProfile({displayName: name}).then(() => {
+          if (updateProfileError)
+            useDeleteUser(auth);
+          else
+            navigation.navigate('Home');
+        })
+      }
     });
+    if (createUserError || updateProfileError)
+      setErrorMessage(errorByCode.get(createUserError.code) ?? 'Unknown Error');
+    else
+      setErrorMessage(undefined);
   };
 
   return (
@@ -140,9 +73,9 @@ const SignUp: React.FC<SignUpProps> = ({navigation}) => {
         </Text>
         <View style={{gap: 30}}>
           <View style={{gap: 10}}>
-            {(error || pwdNotEqualError) &&
+            {errorMessage !== undefined &&
                 <Text style={[styles('error'), {alignSelf: 'center'}]}>
-                  {pwdNotEqualError ? 'Passwords do not match' : errorByCode.get(error.code) ?? 'Unknown Error'}
+                  {errorMessage}
                 </Text>
             }
             <View style={styles('inputWrapper')}>
@@ -162,7 +95,7 @@ const SignUp: React.FC<SignUpProps> = ({navigation}) => {
               <TextInput style={styles('textInput')} secureTextEntry onChangeText={setRepeatPassword}/>
             </View>
           </View>
-          <Button style={{alignSelf: 'center'}} text='Sign Up' loading={loading} onPress={signUp}/>
+          <Button style={{alignSelf: 'center'}} text='Sign Up' loading={loading || updating} onPress={signUp}/>
         </View>
         <View style={{flexDirection: 'row', gap: 3}}>
           <Text style={styles('text')}>Already have an account?</Text>
@@ -176,7 +109,6 @@ const SignUp: React.FC<SignUpProps> = ({navigation}) => {
       <StatusBar style='auto'/>
     </View>
   );
->>>>>>> Stashed changes
 };
 
 export default SignUp;
