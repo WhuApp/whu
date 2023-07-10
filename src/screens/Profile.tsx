62b991833c83a7dc4b34a0f105fb5d98ac77d7dd
Nils <imrebux@gmail.com>
2023-07-10 13:05:40 +0200

    :lipstick: Fix some invalid UI things (#21)

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,31 +1,28 @@
-import React, { useState } from 'react';
-import { useColorScheme, View } from 'react-native';
+import React from 'react';
+import { useColorScheme, View, Text, TouchableOpacity } from 'react-native';
 import { useAuth } from '../components/AuthContext';
 import { getStyles, Elements } from '../styles';
 import { StatusBar } from 'expo-status-bar';
-import { Button, InsetView } from '../components';
+import { Icon, InsetView } from '../components';
 
 const Profile: React.FC = () => {
   const { signOut } = useAuth();
 
-  const [loading, setLoading] = useState<boolean>(false);
-
   const colorScheme = useColorScheme();
   const styles = (element: keyof Elements) => getStyles(element, colorScheme);
 
-  const handleSignOut = () => {
-    setLoading(true);
-    signOut().then(() => setLoading(false));
-  };
-
   return (
-    <View style={styles('page')}>
+    <View style={[styles('page'), { padding: 15 }]}>
       <InsetView>
-        <Button
-          text='Sign out'
-          loading={loading}
-          onPress={handleSignOut}
-        />
+        <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
+        <Text style={styles('title')}>Profile</Text>
+          <TouchableOpacity
+            style={styles('secondaryButton')}
+            onPress={signOut}
+          >
+            <Icon name='log-out' />
+          </TouchableOpacity>
+        </View>
       </InsetView>
       <StatusBar style='auto' />
     </View>
--- src/types.ts
+++ src/types.ts
@@ -7,4 +7,5 @@ export type RootStackParamList = {
   Home: undefined;
   AddFriends: undefined;
   Profile: undefined;
+  Settings: undefined;
 };
