9b4777bb68c52d6b43de3a75ce9dea061d6c850d
gerbernoah <noahgerber100@gmail.com>
2023-07-19 20:44:39 +0200

    implemented setup-user function

--- src/components/AuthContext.tsx
+++ src/components/AuthContext.tsx
@@ -1,21 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import {
   Account,
-  Databases,
   ID,
   Models,
-  Permission,
-  Role,
+  Functions,
 } from 'appwrite';
 import { currentTimedLocationAsync } from '../location';
 import { client } from '../appwrite';
 
-const DATABASE_ID = '6488df9565380dad0d54';
-const COLLECTION_LOCATION_ID = '649df6a988dfc4a3026e';
+const FUNCTION_SETUP_USER_ID = '64b820cd88e01612a887';
 
+const functions = new Functions(client);
 const account = new Account(client);
-const databases = new Databases(client);
-
 const AuthContext = React.createContext(null);
 
 const AuthProvider: React.FC<React.PropsWithChildren> = ({ children }) => {
@@ -38,25 +34,16 @@ const AuthProvider: React.FC<React.PropsWithChildren> = ({ children }) => {
   };
 
   const signUp = async (name: string, mail: string, password: string) => {
-    const user = await account.create(ID.unique(), mail, password, name);
+    await account.create(ID.unique(), mail, password, name);
     const session = await account.createEmailSession(mail, password);
 
-    await createUserDocument(user.$id);
+    const payload = JSON.stringify({ data: await currentTimedLocationAsync() });
+    const execution = await functions.createExecution(FUNCTION_SETUP_USER_ID, payload);
+    const data = JSON.parse(execution.response);
+  
+    if (!data.success) return data.message;
+    
     setSession(session);
-  };
-
-  // TODO: Create a appwrite function for signup proccess
-  const createUserDocument = async (id: string) => {
-    const data = await currentTimedLocationAsync();
-    const permissions = [
-      Permission.read(Role.user(id)),
-      Permission.update(Role.user(id)),
-      Permission.delete(Role.user(id)),
-      Permission.write(Role.user(id)),
-    ];
-    const document = await databases.createDocument(DATABASE_ID, COLLECTION_LOCATION_ID, id, data, permissions);
-
-    return document;
   };
 
   const auth = {
