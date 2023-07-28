f1c1695505930188a2bed54c9843eb8816571fc0
gerbernoah <noahgerber100@gmail.com>
2023-07-28 18:55:49 +0200

    implemented user-exists function

---
+++ functions/user-exists/src/index.js
@@ -0,0 +1,50 @@
+const { Client, Users, Query } = require('node-appwrite');
+
+const userExists = async function (request, response) {
+  // Check if everything is set up correctly
+  if (
+    [
+      'APPWRITE_FUNCTION_ENDPOINT',
+      'APPWRITE_FUNCTION_PROJECT_ID',
+      'APPWRITE_FUNCTION_API_KEY',
+    ].some((x) => !request.variables[x])
+  ) {
+    throw new Error('Some variables are missing');
+  }
+
+  const client = new Client();
+  const users = new Users(client);
+
+  client
+    .setEndpoint(request.variables['APPWRITE_FUNCTION_ENDPOINT'])
+    .setProject(request.variables['APPWRITE_FUNCTION_PROJECT_ID'])
+    .setKey(request.variables['APPWRITE_FUNCTION_API_KEY']);
+
+  if (!request.payload) {
+    throw new Error('No payload provided');
+  }
+
+  const payload = JSON.parse(request.payload);
+
+  if (!payload.data) {
+    throw new Error('No data provided');
+  }
+
+  const user = payload.data;
+  console.log('user provided: ', user);
+
+  const machingName = await users.list([Query.equal('name', [user.name])]);
+  const machingMail = await users.list([Query.equal('email', [user.email])]);
+  const machingUsers = [].concat(machingName.users).concat(machingMail.users);
+
+  if (machingUsers.length === 0) {
+    console.log('user not found');
+    return response.json({});
+  } else if (machingUsers.length > 1) {
+    console.error('More than one user share unique values', user);
+  }
+  console.log('user(s) found:', [...machingUsers]);
+  return response.json({ user: machingUsers[0] });
+};
+
+module.exports = userExists;
