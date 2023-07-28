91cbdd7e94e597d2f6ab4dc7c6f1677212b14fdf
gerbernoah <noahgerber100@gmail.com>
2023-07-28 18:52:56 +0200

    implement jest testing

--- src/appwrite.ts
+++ src/appwrite.ts
@@ -1,8 +1,6 @@
 import { Client } from 'appwrite';
 
-const ENDPOINT = 'https://cloud.appwrite.io/v1';
-const PROJECT_ID = '648644a80adadf63b7d4';
+export const ENDPOINT = 'https://cloud.appwrite.io/v1';
+export const PROJECT_ID = '648644a80adadf63b7d4';
 
-export const client = new Client()
-  .setEndpoint(ENDPOINT)
-  .setProject(PROJECT_ID);
+export const client = new Client().setEndpoint(ENDPOINT).setProject(PROJECT_ID);
---
+++ tests/functions/userExists.test.js
@@ -0,0 +1,26 @@
+import { Functions, Client, Account } from 'appwrite';
+import { ENDPOINT, PROJECT_ID } from '../../src/appwrite';
+
+const USER_EXISTS_ID = '64c3b745e7e1fa7c9445';
+const user = {
+  id: '64c3d3253309cb3dfadf',
+  email: 'userExists01@test.com',
+  password: 'userExists01.Password',
+  name: 'userExists01',
+};
+const client = new Client().setEndpoint(ENDPOINT).setProject(PROJECT_ID);
+const functions = new Functions(client);
+
+test('user mail check', async () => {
+  const payload = JSON.stringify({ data: { email: user.email } });
+  const execution = await functions.createExecution(USER_EXISTS_ID, payload);
+  const data = JSON.parse(execution.response);
+  expect(data.user.$id).toBe(user.id);
+});
+
+test('user name check', async () => {
+  const payload = JSON.stringify({ data: { name: user.name } });
+  const execution = await functions.createExecution(USER_EXISTS_ID, payload);
+  const data = JSON.parse(execution.response);
+  expect(data.user.$id).toBe(user.id);
+});
