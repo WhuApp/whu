91b6d30d2e31bae92c3f3dcb943150de3d2915b4
7rebux <58339645+7rebux@users.noreply.github.com>
2023-08-02 14:57:18 +0200

    :construction: Add more register tests

--- tests/functions/register.test.js
+++ tests/functions/register.test.js
@@ -1,54 +1,113 @@
 import { register } from '../../src/api/functions';
 
-const validUser = {
+const nonExistentCredentials = {
   id: '64c445ccb399021ee62f',
   email: 'userRegister01@test.com',
   password: 'userRegister01.Password',
-  name: 'userRegister01',
+  username: 'userRegister01',
 };
 
-const invalidUser = {
-  email: 'a',
-  password: 'a',
-  name: 'a',
+const existentCredentials = {
+  id: '64c997a6bea13a0a1fcf',
+  username: 'userLogin01',
+  email: 'userlogin01@test.com',
+  password: 'userLogin01.Password',
 };
 
-test('valid user (maybe already existent)', async () => {
-  const response = await register({
-    email: validUser.email,
-    name: validUser.name,
-    password: validUser.password,
-  });
-  expect(response.success).toBe(false);
-  expect(response.error).toBe('Name is unavailable');
+test('Register with non existent valid credentials', async () => {
+  // TODO implement
+  expect(true).toBeFalsy();
 });
 
-test('invalid email', async () => {
+test('Register with invalid email', async () => {
   const response = await register({
-    email: invalidUser.email,
-    name: validUser.name,
-    password: validUser.password,
+    email: 'x',
+    name: nonExistentCredentials.username,
+    password: nonExistentCredentials.password,
   });
-  expect(response.success).toBe(false);
+
+  expect(response.success).toBeFalsy();
   expect(response.error).toBe('Invalid email');
+  expect(response.data).toBeUndefined();
 });
 
-test('invalid name', async () => {
+test('Register with existent email', async () => {
   const response = await register({
-    email: validUser.email,
-    name: invalidUser.name,
-    password: validUser.password,
+    email: existentCredentials.email,
+    name: nonExistentCredentials.username,
+    password: nonExistentCredentials.password,
   });
-  expect(response.success).toBe(false);
+
+  expect(response.success).toBeFalsy();
+  expect(response.error).toBe('Email already registered');
+  expect(response.data).toBeUndefined();
+});
+
+test('Register with invalid username', async () => {
+  const response = await register({
+    email: nonExistentCredentials.email,
+    name: 'x',
+    password: nonExistentCredentials.password,
+  });
+
+  expect(response.success).toBeFalsy();
   expect(response.error).toBe('Invalid username');
+  expect(response.data).toBeUndefined();
 });
 
-test('invalid password', async () => {
+test('Register with existent username', async () => {
   const response = await register({
-    email: validUser.email,
-    name: validUser.name,
-    password: invalidUser.password,
+    email: nonExistentCredentials.email,
+    name: existentCredentials.username,
+    password: nonExistentCredentials.password,
   });
-  expect(response.success).toBe(false);
+
+  expect(response.success).toBeFalsy();
+  expect(response.error).toBe('Name is unavailable');
+  expect(response.data).toBeUndefined();
+});
+
+test('Register with invalid password', async () => {
+  const response = await register({
+    email: nonExistentCredentials.email,
+    name: nonExistentCredentials.username,
+    password: 'x',
+  });
+
+  expect(response.success).toBeFalsy();
   expect(response.error).toBe('Invalid password');
+  expect(response.data).toBeUndefined();
+});
+
+test('Register without email', async () => {
+  const response = await register({
+    name: nonExistentCredentials.username,
+    password: nonExistentCredentials.password,
+  });
+
+  expect(response.success).toBeFalsy();
+  expect(response.error).toBe('Missing email');
+  expect(response.data).toBeUndefined();
+});
+
+test('Register without username', async () => {
+  const response = await register({
+    email: nonExistentCredentials.email,
+    password: nonExistentCredentials.password,
+  });
+
+  expect(response.success).toBeFalsy();
+  expect(response.error).toBe('Missing username');
+  expect(response.data).toBeUndefined();
+});
+
+test('Register without password', async () => {
+  const response = await register({
+    email: nonExistentCredentials.email,
+    name: nonExistentCredentials.username,
+  });
+
+  expect(response.success).toBeFalsy();
+  expect(response.error).toBe('Missing password');
+  expect(response.data).toBeUndefined();
 });
