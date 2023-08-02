8181821e7686e4687ec6112e18f04ac6f492eaa6
7rebux <58339645+7rebux@users.noreply.github.com>
2023-08-02 15:13:28 +0200

    :white_check_mark: Add more user-exists tests

--- tests/functions/userExists.test.js
+++ tests/functions/userExists.test.js
@@ -1,26 +1,55 @@
 import { userExists } from '../../src/api/functions';
 
-const user = {
-  id: '64c442b0f159796f9062',
-  email: 'userExists01@test.com',
-  password: 'userExists01.Password',
-  name: 'userExists01',
+const nonExistentCredentials = {
+  id: '64c445ccb399021ee62f',
+  email: 'userRegister01@test.com',
+  password: 'userRegister01.Password',
+  username: 'userRegister01',
 };
 
-test('user mail test', async () => {
-  const response = await userExists({ emailOrName: user.email });
-  expect(response.success).toBe(true);
-  expect(response.data.exists).toBe(true);
+const existentCredentials = {
+  id: '64c997a6bea13a0a1fcf',
+  username: 'userLogin01',
+  email: 'userlogin01@test.com',
+  password: 'userLogin01.Password',
+};
+
+test('Check existent email', async () => {
+  const response = await userExists({ emailOrName: existentCredentials.email });
+
+  expect(response.success).toBeTruthy();
+  expect(response.error).toBeUndefined();
+  expect(response.data.exists).toBeTruthy();
 });
 
-test('user name test', async () => {
-  const response = await userExists({ emailOrName: user.name });
-  expect(response.success).toBe(true);
-  expect(response.data.exists).toBe(true);
+test('Check existent name', async () => {
+  const response = await userExists({ emailOrName: existentCredentials.username });
+
+  expect(response.success).toBeTruthy();
+  expect(response.error).toBeUndefined();
+  expect(response.data.exists).toBeTruthy();
 });
 
-test('wrong data test', async () => {
-  const response = await userExists({ emailOrName: 'n/A' });
-  expect(response.success).toBe(true);
-  expect(response.data.exists).toBe(false);
+test('Check non existent email', async () => {
+  const response = await userExists({ emailOrName: nonExistentCredentials.email });
+
+  expect(response.success).toBeTruthy();
+  expect(response.error).toBeUndefined();
+  expect(response.data.exists).toBeFalsy();
+});
+
+test('Check non existent name', async () => {
+  const response = await userExists({ emailOrName: nonExistentCredentials.username });
+
+  expect(response.success).toBeTruthy();
+  expect(response.error).toBeUndefined();
+  expect(response.data.exists).toBeFalsy();
+});
+
+test('Check without email or name', async () => {
+  const response = await userExists({});
+
+  expect(response.success).toBeFalsy();
+  expect(response.error).toBe('Missing email or name');
+  expect(response.data).toBeUndefined();
 });
