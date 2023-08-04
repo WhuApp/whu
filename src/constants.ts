c685696e3ab1a1013ffb2fb2dd6f5085b9b5d23e
Nils Osswald <imrebux@gmail.com>
2023-08-04 18:00:40 +0200

    :fire: Removed unused constants

--- src/constants.ts
+++ src/constants.ts
@@ -6,7 +6,5 @@ export const FUNCTION_IDS = {
   addFriend: '64aef40f8bc33879bb25',
   removeFriend: '64b67d44e75c49e2110e',
   getFriendRequests: '64b645dbb12660ad9258',
-  register: '64c4123000a5be24fa29',
   userExists: '64c3b745e7e1fa7c9445',
-  login: '64c9894b1ec13aa4db59',
 };
