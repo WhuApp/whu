a0ad771f987fd208beab59fbdfc0677f13743105
Kai Jellinghaus <contact@kaij.tech>
2023-08-18 23:18:43 +0200

    Add Credentials to Dev Page

--- src/screens/DevPage.tsx
+++ src/screens/DevPage.tsx
@@ -1,12 +1,20 @@
-import React from "react";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React, { useEffect, useMemo, useState } from "react";
+import {
+  ActivityIndicator,
+  FlatList,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  VirtualizedList,
+} from "react-native";
 import { useAuth0 } from "react-native-auth0";
 import { useColors } from "../utils";
 import { ModalLayout } from "../layouts";
 import { useClipboard } from "@react-native-community/clipboard";
 
-const Code: React.FC<React.PropsWithChildren> = (children) => {
-  const c = children.children!.toString();
+const Code: React.FC<React.PropsWithChildren> = ({ children }) => {
+  const c = children!.toString();
   const colors = useColors();
   const [_clipboard, setClipboard] = useClipboard();
 
@@ -27,16 +35,15 @@ const Code: React.FC<React.PropsWithChildren> = (children) => {
   );
 };
 
-const DevPage: React.FC = () => {
-  const { user } = useAuth0();
+const ObjectView: React.FC<{ obj: unknown }> = ({ obj }) => {
+  if (!obj) {
+    return <ActivityIndicator></ActivityIndicator>;
+  }
+
+  const s = useMemo(() => Object.entries(obj), []);
   const colors = useColors();
 
   const styles = StyleSheet.create({
-    textList: {
-      display: "flex",
-      flexDirection: "column",
-      gap: 10,
-    },
     empty: {
       flexDirection: "row",
       gap: 20,
@@ -45,15 +52,65 @@ const DevPage: React.FC = () => {
       color: colors("textPrimary"),
     },
   });
+  return (
+    <VirtualizedList
+      data={s}
+      initialNumToRender={s.length}
+      renderItem={({ item }) => {
+        const name = item[0];
+        const content = item[1];
+        return (
+          <View style={styles.empty}>
+            <Text style={styles.text}>
+              {name}
+            </Text>
+            {!content && <Code>null</Code>}
+            {(content && typeof (content) === "string") && (
+              <Code>{content}</Code>
+            )}
+            {(content && typeof (content) === "number") && (
+              <Code>{(content as number).toString()}</Code>
+            )}
+            {(content && typeof (content) === "object") && (
+              <ObjectView obj={content} />
+            )}
+          </View>
+        );
+      }}
+      keyExtractor={(item) => item[0]}
+      getItemCount={(o) => o.length}
+      getItem={(o, i) => o[i]}
+    />
+  );
+};
+
+const DevPage: React.FC = () => {
+  const { getCredentials, user } = useAuth0();
+  const colors = useColors();
+  const [credentials, setCredentials] = useState(undefined);
+
+  useEffect(() => {
+    getCredentials().then((c) => setCredentials(c));
+  }, [user]);
+
+  const styles = StyleSheet.create({
+    textList: {
+      display: "flex",
+      flexDirection: "column",
+      gap: 10,
+    },
+    title: {
+      color: colors("textPrimary"),
+      fontSize: 25,
+    },
+  });
 
   return (
     <ModalLayout title={"Dev Page"} onPressMore={() => {}}>
       <View style={styles.textList}>
-        <View style={styles.empty}>
-          <Text style={styles.text}>
-            JWT Token:
-          </Text>
-          <Code>Test</Code>
+        <View>
+          <Text style={styles.title}>Credentials</Text>
+          <ObjectView obj={credentials}></ObjectView>
         </View>
       </View>
     </ModalLayout>
