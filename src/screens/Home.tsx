fcd4ef4b43efcc68b0a252f9265ec7b887324379
Nils Osswald <contact@nilsosswald.de>
2023-10-11 21:39:32 +0200

    :recycle: Display username on profile

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,14 +1,9 @@
 import React from 'react';
-import { Text } from 'react-native';
-import { useAuth0 } from 'react-native-auth0';
 import { FriendList } from '../components';
 
 const Home: React.FC = () => {
-  const { user } = useAuth0();
-
   return (
     <>
-      <Text>Logged in as {user.name}</Text>
       <FriendList />
     </>
   );
