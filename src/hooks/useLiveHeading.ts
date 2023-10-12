31e283cd6a9b83add67b833cc0bf41cc1f3dbf2d
Nils Osswald <contact@nilsosswald.de>
2023-10-12 16:12:35 +0200

    :zap: Update live location & heading hooks

--- src/hooks/useLiveHeading.ts
+++ src/hooks/useLiveHeading.ts
@@ -1,18 +1,36 @@
 import { useEffect, useState } from 'react';
-import { watchHeadingAsync } from 'expo-location';
-import type { LocationHeadingObject } from 'expo-location';
+import { watchHeadingAsync, LocationHeadingObject, LocationSubscription } from 'expo-location';
+
+const SMOOTHNESS = 10; // the lower, the smoother
+const BUFFER_SIZE = 8; // the lower, the more accurate
 
 // DANGER!!! This hook expects to already have permissions to get foreground location data
 const useLiveHeading = () => {
-  const [heading, setHeading] = useState<number>();
+  const [subscription, setSubscription] = useState<LocationSubscription>();
+  const [headingBuffer, setHeadingBuffer] = useState<number[]>([]);
+  const [heading, setHeading] = useState<number>(0);
 
   const onHeadingUpdate = (data: LocationHeadingObject) => {
-    setHeading(data.trueHeading);
+    setHeadingBuffer((prev) => [...prev, data.trueHeading]);
   };
 
   useEffect(() => {
+    const averageHeading = headingBuffer.reduce((acc, val) => acc + val, 0) / headingBuffer.length;
+
+    if (headingBuffer.length > BUFFER_SIZE) {
+      setHeadingBuffer(headingBuffer.slice(1));
+    }
+
+    if (Math.abs(heading - averageHeading) > SMOOTHNESS) {
+      setHeading(averageHeading);
+    }
+  }, [headingBuffer]);
+
+  useEffect(() => {
     (async () => {
-      const subscription = await watchHeadingAsync(onHeadingUpdate);
+      if (!subscription) {
+        setSubscription(await watchHeadingAsync(onHeadingUpdate));
+      }
 
       return () => subscription.remove();
     })();
