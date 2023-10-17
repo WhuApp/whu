cfb04d0578d344646fcf7097a4edf09305fcdd79
Nils Osswald <contact@nilsosswald.de>
2023-10-17 19:07:06 +0200

    :heavy_plus_sign: Add @tanstack/react-query

--- src/Root.tsx
+++ src/Root.tsx
@@ -2,12 +2,17 @@ import React from 'react';
 import { Auth0Provider } from 'react-native-auth0';
 import { AUTH0_CLIENT_ID, AUTH0_DOMAIN } from './constants';
 import App from './App';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const Root: React.FC = () => {
+  const queryClient = new QueryClient();
+
   return (
-    <Auth0Provider domain={AUTH0_DOMAIN} clientId={AUTH0_CLIENT_ID}>
-      <App />
-    </Auth0Provider>
+    <QueryClientProvider client={queryClient}>
+      <Auth0Provider domain={AUTH0_DOMAIN} clientId={AUTH0_CLIENT_ID}>
+        <App />
+      </Auth0Provider>
+    </QueryClientProvider>
   );
 };
 
