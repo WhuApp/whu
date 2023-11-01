2051643b1838dd11d02a5cc23a6a19e4c44f3de5
Nils Osswald <contact@nilsosswald.de>
2023-11-01 22:49:59 +0100

    💬 Format last update time (#51)
    * :heavy_plus_sign: Add dayjs
    
    * :speech_balloon: Format last update time

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 import { registerRootComponent } from 'expo';
 import Root from './src/Root';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+
+dayjs.extend(relativeTime);
 
 registerRootComponent(Root);
